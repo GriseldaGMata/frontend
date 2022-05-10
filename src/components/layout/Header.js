524bd611ec14dfced097c735d0a59d849df5ada2
GriseldaGMata <griseldadelamata@gmail.com>
2022-05-10 22:33:00 +0200

    ok

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,14 +4,16 @@ import React from 'react';
 const Header = (props) => {
     return (
         <header>
-            <div className="holder">
+            <div className="holder , shadow-lg p-3 mb-5 bg-body rounded">
                 <div className="logo">
-                    <img src='img/logogestoria.jpeg' width="100"
+                    <img src='img/logogestoria.jpeg' width="150"
                         alt="Gestoria Mata" />
-                    <h1>Gestoria Mata - soluciones integrales</h1>
+                    <h1> Gestoria Mata - soluciones integrales </h1>
                 </div>
             </div>
         </header>
     );
 }
 export default Header;
+
+<div class="shadow-lg p-3 mb-5 bg-body rounded">Larger shadow</div>
