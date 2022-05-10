d148efe4d279cdf84b507375ca60871d1596f814
GriseldaGMata <griseldadelamata@gmail.com>
2022-05-10 18:11:10 +0200

    Migracion REACTJS con estilos

--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,13 +1,36 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import NovedadItem from '../components/novedades/NovedadItem';
+
 import '../styles/components/pages/NovedadesPage.css';
 
-import React from "react";
 const NovedadesPage = (props) => {
+    const [loading, setLoading]=useState(false);
+    const [novedades, setNovedades]=useState([]);
+
+    useEffect(()=>{
+        const cargarNovedades=async ()=>{
+            setLoading(true);
+            const response=await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+        cargarNovedades();
+    }, []);
+    
+
     return (
         <section className="holder">
             <h2>Novedades</h2>
-            <h3>titulo</h3>
-            <h4>subtitulo</h4>
-            <p>cuerpo</p>
+            {loading ? (
+                <p>Cargando, por favor aguarde...</p>
+            ) : (
+                novedades.map(item => <NovedadItem key={item.id} 
+                    title={item.titulo} 
+                    subtitle={item.subtitulo} 
+                    imagen={item.imagen} 
+                    body={item.cuerpo} />)
+            )}
         </section>        
     );
 }
