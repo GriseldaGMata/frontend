98bd6613430375c6eac06e518b1a6abaf65045e9
GriseldaGMata <griseldadelamata@gmail.com>
2022-05-13 12:13:00 +0200

    estilo contacto

--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -4,48 +4,55 @@ import React from "react";
 const ContactoPage = (props) => {
     return (
         <main className="holder">
-        <div className="columna">
-            <div className="columnaizquierda">
-                <h2>Complete el siguiente formulario</h2>
-                <form action="" method="" className="formulario">
-                    <p>
-                        <label>Nombre</label>
-                        <input type="text" name="nombre" />
-                    </p>
-                    <p>
-                        <label>Email</label>
-                        <input type="text" name="email" />
-                    </p>
-                    <p>
-                        <label>Telefono</label>
-                        <input type="text" name="telefono" />
-                    </p>
-                    <p>
-                        <label>Comentario</label>
-                        <textarea name="mensaje"></textarea> 
-                    </p>
-                    <p className="centrar">
-                        <input type="submit" value="Enviar" />
-                    </p>
-                </form>
-            </div>
-       
-           <div className="columnaderecha">
-                  <h2>Otras vias de contacto</h2>
-                  <p>Si lo desea puede contactarnos utilizando los siguientes medios:</p>
-                  <ul>
-                      <li>Telefono: 123 1234 123</li>
-                      <li>Email: </li>
-                      <li>Facebook</li>
-                      <li>Twitter</li>
-                      <li>Skype</li>
-                      <li>instagram</li>
-                      <li>Nuestras oficinas en: calle Siempre viva 12345-Malaga</li>
-                  </ul>
-            </div>
+            <div class="container">
+                <div class="box">
+                    <div class="col">
+                        <div className="izquierda">
+                            <h2>Complete el siguiente formulario</h2>
+                            <form action="" method="" className="formulario">
+                                <p>
+                                    <label>Nombre completo:</label>
+                                    <input type="text" name="nombre" />
+                                </p>
+                                <p>
+                                    <label>Email:</label>
+                                    <input type="text" name="email" />
+                                </p>
+                                <p>
+                                    <label>Telefono o movil:</label>
+                                    <input type="text" name="telefono" />
+                                </p>
+                                <p>
+                                    <label>Comentario:</label>
+                                    <textarea name="mensaje"></textarea>
+                                </p>
+                                <p className="centrar">
+                                    <input type="submit"  value="Enviar datos: Nos contactaremos a la brevedad." className='boton'/>
+                                    
 
-        </div>     
-       </main>
+                                </p>
+                            </form>
+
+                        </div>
+                        <div class="col">
+                            <div className="derecha">
+                                <h2>Otras vias de contacto</h2>
+                                <p>Si lo desea puede contactarnos utilizando los siguientes medios:</p>
+                                <ul>
+                                    <li>Telefono: 123 1234 123</li>
+                                    <li>Email: </li>
+                                    <li>Facebook</li>
+                                    <li>Twitter</li>
+                                    <li>Skype</li>
+                                    <li>instagram</li>
+                                    <li>Nuestras oficinas en: calle Siempre viva 12345-Malaga</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 export default ContactoPage;
